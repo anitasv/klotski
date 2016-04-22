f7091892349c0653d5b945bbad56b470f8f69722
Anita Vasu <anita.vasu@inmobi.com>
2016-04-22 10:56:26 +0530

    Fix for DCDL

--- src/main/java/me/anitas/DynCountDownLatch.java
+++ src/main/java/me/anitas/DynCountDownLatch.java
@@ -4,6 +4,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
+/**
+ * Rather inefficient, but correct implementation of a dynamic count down latch.
+ *
+ * I recommend to use Phaser, or usual CountDownLatch if it works for you.
+ *
+ * The advantage this has over Phaser is that it supports more than one
+ * party. Disadvantage wise even though it behaves like a phaser, no phase
+ * number is returned.
+ */
 public class DynCountDownLatch  {
 
     private final AtomicInteger internal = new AtomicInteger(1);
@@ -30,7 +39,7 @@ public class DynCountDownLatch  {
         countDown();
 
         lock.lock();
-        while (internal.get() != 0){
+        while (internal.get() != 0) {
             condition.await();
         }
         lock.unlock();
